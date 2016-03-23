31705272560272ddf994601cbfc53ada0c65a1fa
joshuajnet <joshuajohnson@Joshuas-MacBook-Air.local>
2016-03-22 22:52:49 -0400

    improved js

--- js/raindrops.js
+++ js/raindrops.js
@@ -14,14 +14,12 @@ $(document).ready(function() {
 	    var posx = (Math.random() * (winwidth-100)).toFixed();
 		var posy = (Math.random() * (winheight-100)).toFixed();
 		
-
-		
 		Posdiv.css({
 		        'left':posx+'px',
 		        'top':posy+'px',
-		    }).toggleClass('active')
-		    
-	    }, rand);
+		    }).addClass('active').delay(1500).queue(function(){
+				Posdiv.removeClass('active').dequeue();
+			});
 	   
     }
       
